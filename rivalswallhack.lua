5781fdaad7b9a9f5db7f5ff73021a877a5d38caa
yNyvek13 <ynyvek13@gmail.com>
2025-01-03 12:40:40 -0300

    Create rivalswallhack.lua

---
+++ rivalswallhack.lua
@@ -0,0 +1,8 @@
+local hl = Instance.new("Highlight")
+hl.FillTransparency = 1
+hl.OutlineColor = Color3.fromRGB(255, 0, 255)
+hl.OutlineTransparency = 0
+
+for i, v in pairs(game["Players - Client"]:GetPlayers()) do
+    hl:Clone().Parent = v.Character
+end
