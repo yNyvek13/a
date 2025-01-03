e01966d4673dc53dd5f9df735c0caa27cbf44e44
yNyvek13 <ynyvek13@gmail.com>
2025-01-03 12:40:05 -0300

    Update and rename script.lua to gunfightwallhack.lua

--- gunfightwallhack.lua
+++ gunfightwallhack.lua
@@ -3,6 +3,6 @@ hl.FillTransparency = 1
 hl.OutlineColor = Color3.fromRGB(255, 0, 255)
 hl.OutlineTransparency = 0
 
-for i, v in pairs(game["Players - Client"]:GetPlayers()) do
+for i, v in pairs(game.Players:GetPlayers()) do
     hl:Clone().Parent = v.Character
 end
