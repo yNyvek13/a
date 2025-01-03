62e60ad2fd5829c333e78fea20567190797e12ad
yNyvek13 <ynyvek13@gmail.com>
2025-01-03 18:01:54 -0300

    Delete gunfightwallhack.lua

--- gunfightwallhack.lua
+++
@@ -1,8 +0,0 @@
-local hl = Instance.new("Highlight")
-hl.FillTransparency = 1
-hl.OutlineColor = Color3.fromRGB(255, 0, 255)
-hl.OutlineTransparency = 0
-
-for i, v in pairs(game.Players:GetPlayers()) do
-    hl:Clone().Parent = v.Character
-end
