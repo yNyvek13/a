43ac6f00a1f334393ed47ba070edae575aa1f4ea
yNyvek13 <ynyvek13@gmail.com>
2025-01-03 00:05:22 -0300

    script.lua

--- script.lua
+++ script.lua
@@ -1,11 +1,8 @@
 local hl = Instance.new("Highlight")
-hl.FillColor = Color3.fromRGB(255, 0, 255)
-hl.FillTransparency = 0.25
+hl.FillTransparency = 1
 hl.OutlineColor = Color3.fromRGB(255, 0, 255)
 hl.OutlineTransparency = 0
 
-for i, v in pairs(game.Players:GetPlayers()) do
-	if v.Name ~= "kevindarcico125" and "kaus_Y8" then
-		hl:Clone().Parent = v.Character
-	end
+for i, v in pairs(game["Players - Client"]:GetPlayers()) do
+    hl:Clone().Parent = v.Character
 end
