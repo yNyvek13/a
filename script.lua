984719d37d3cbf44a1b112815bdb65de4e6df232
yNyvek13 <ynyvek13@gmail.com>
2025-01-02 23:37:04 -0300

    script.lua

---
+++ script.lua
@@ -0,0 +1,11 @@
+local hl = Instance.new("Highlight")
+hl.FillColor = Color3.fromRGB(255, 0, 255)
+hl.FillTransparency = 0.25
+hl.OutlineColor = Color3.fromRGB(255, 0, 255)
+hl.OutlineTransparency = 0
+
+for i, v in pairs(game.Players:GetPlayers()) do
+	if v.Name ~= "kevindarcico125" then
+		hl:Clone().Parent = v.Character
+	end
+end
