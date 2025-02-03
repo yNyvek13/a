1c68f2dd730d347d1ad9e24a13b8ae56cda28977
yNyvek13 <ynyvek13@gmail.com>
2025-02-03 03:02:09 -0300

    Update bluelockkaiserstyle.lua

--- bluelockkaiserstyle.lua
+++ bluelockkaiserstyle.lua
@@ -1,27 +1,30 @@
 local plr = game.Players.LocalPlayer
-local Background = plr.PlayerGui.Style.BG
+local Stats = plr.PlayerStats
+local style = Stats.Style
+local plrgui = plr.PlayerGui
+local skills = plrgui.InGameUI.Bottom.Abilities
+local Background = plrgui.Style.BG
 local desc = Background.Desc
 local txt = Background.StyleTxt
 local slots = Background.Slots.ScrollingFrame
 local slot = slots.Slot1
 local rarities = Background.Rarities.ScrollingFrame
-local epic = rarities["500 Legendary"]:Clone()
-if not rarities:FindFirstChild("700 World") then
-    epic.Name = "700 World"
-    epic.Parent = rarities
-end
-local WorldClass = rarities["700 World"]
-
-WorldClass.Image = rarities["400 Epic"].Image
-WorldClass.ImageColor3 = Color3.new(0,255,255)
-WorldClass.Text.Text = "WORLD (0.05%)"
 
 while true do
     task.wait()
+    style.Value = "Kaiser"
     desc.Text = "The Emperor"
     slot.Image = rarities["400 Epic"].Image
     slot.ImageColor3 = Color3.new(0,255,255)
     txt.Text = "Kaiser"
     txt.TextColor3 = Color3.new(255,255,255)
     slot.Text.Text = "Kaiser"
+    skills["1"].Timer.Text = "Kaiser Impact"
+    skills["2"].Timer.Text = "Godly Dribble"
 end
+
+if skills["1"]:FindFirstChild("Cooldown") or skills["2"]:FindFirstChild("Cooldown") then
+    skills["2"]:FindFirstChild("Cooldown"):Destroy()
+    skills["1"]:FindFirstChild("Cooldown"):Destroy()
+end
+
