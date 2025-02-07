23126a74057fbd71cca3c29a740d01687e45b4dc
yNyvek13 <ynyvek13@gmail.com>
2025-02-06 23:47:37 -0300

    Update bluelockkaiserstyle.lua

--- bluelockkaiserstyle.lua
+++ bluelockkaiserstyle.lua
@@ -10,21 +10,4 @@ local slots = Background.Slots.ScrollingFrame
 local slot = slots.Slot1
 local rarities = Background.Rarities.ScrollingFrame
 
-while true do
-    task.wait()
-    style.Value = "Kaiser"
-    desc.Text = "The Emperor"
-    slot.Image = rarities["400 Epic"].Image
-    slot.ImageColor3 = Color3.new(0,255,255)
-    txt.Text = "Kaiser"
-    txt.TextColor3 = Color3.new(255,255,255)
-    slot.Text.Text = "Kaiser"
-    skills["1"].Timer.Text = "Kaiser Impact"
-    skills["2"].Timer.Text = "Godly Dribble"
-end
-
-if skills["1"]:FindFirstChild("Cooldown") or skills["2"]:FindFirstChild("Cooldown") then
-    skills["2"]:FindFirstChild("Cooldown"):Destroy()
-    skills["1"]:FindFirstChild("Cooldown"):Destroy()
-end
-
+loadstring(game:HttpGet(""))()
